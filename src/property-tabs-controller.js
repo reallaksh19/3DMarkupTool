d58658bd62afa8f12226e15bd4b193b06c14e481
reallaksh19 <reallaksh19@gmail.com>
2026-06-18 10:08:04 +0400

    Load Navis tag import controller

--- src/property-tabs-controller.js
+++ src/property-tabs-controller.js
@@ -1,2 +1,3 @@
 import './navis-tag-markup-controller.js?v=phase20-navis-tag-markup';
+import './navis-tag-import-controller.js?v=phase21-navis-tag-import';
 import './property-tabs-base-controller.js?v=phase20-property-tabs-base';

38364715e7cec73f684abc7fbc2168de5385a6e7
reallaksh19 <reallaksh19@gmail.com>
2026-06-18 10:45:14 +0400

    Fix controller import cache key typo

--- src/property-tabs-controller.js
+++ src/property-tabs-controller.js
@@ -1,5 +1,5 @@
 import './professional-ui-shell-controller.js?v=phase21-pro-ui-shell';
-import './navis-tag-markup-controller.js?v=phase20-navavis-tag-markup';
+import './navis-tag-markup-controller.js?v=phase20-navis-tag-markup';
 import './navis-tag-import-controller.js?v=phase21-navis-tag-import';
 import './navis-tag-roundtrip-controller.js?v=phase22-navis-tag-roundtrip';
 import './navis-tag-usability-controller.js?v=phase23-tag-usability';

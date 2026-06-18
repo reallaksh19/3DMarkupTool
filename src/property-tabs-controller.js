a074917b450f8a768e0bef07779af247587ce86d
reallaksh19 <reallaksh19@gmail.com>
2026-06-18 10:12:30 +0400

    Load Navis tag roundtrip status controller

--- src/property-tabs-controller.js
+++ src/property-tabs-controller.js
@@ -1,3 +1,4 @@
 import './navis-tag-markup-controller.js?v=phase20-navis-tag-markup';
 import './navis-tag-import-controller.js?v=phase21-navis-tag-import';
+import './navis-tag-roundtrip-controller.js?v=phase22-navis-tag-roundtrip';
 import './property-tabs-base-controller.js?v=phase20-property-tabs-base';

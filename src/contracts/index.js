f68f79f3b05b523794788c59c0ac9ea0c188d80c
reallaksh19 <reallaksh19@gmail.com>
2026-06-29 18:15:47 +0400

    Add core contract index

---
+++ src/contracts/index.js
@@ -0,0 +1,25 @@
+export {
+  PLATFORM_CONTRACT_SCHEMA,
+  PLANT_MODEL_GRAPH_SCHEMA,
+  CATALOGUE_REGISTRY_SCHEMA,
+  COMPONENT_CATALOGUE_ITEM_SCHEMA,
+  RESOLVED_PRIMITIVE_MODEL_SCHEMA,
+  CONTRACT_SCHEMA_SET
+} from './platform-contract-schemas.js';
+
+export {
+  validatePlantModelGraphContract,
+  assertPlantModelGraphContract
+} from './plant-model-graph-contract.js';
+
+export {
+  validateCatalogueRegistryContract,
+  validateComponentCatalogueItemContract,
+  assertCatalogueRegistryContract,
+  assertComponentCatalogueItemContract
+} from './catalogue-contract.js';
+
+export {
+  validateResolvedPrimitiveModelContract,
+  assertResolvedPrimitiveModelContract
+} from './resolved-primitive-model-contract.js';
